35b1ae4ba95c5e2112e90f42f474261b61c972ab
YafanChow <90690336+YafanChow@users.noreply.github.com>
2022-12-06 13:43:38 +0100

    1

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -1,16 +1,21 @@
 from flask import Flask
-
+# from flask_cors import CORS
+from flask_swagger_ui import get_swaggerui_blueprint
 # Always use relative import for custom module
 from .package.module import MODULE_VALUE
 
+
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return (
-        "Try /hello/Chris for parameterized Flask route.\n"
-        "Try /module for module import guidance"
-    )
+# @app.route("/")
+# def index():
+#     return (
+#         "Try /hello/Chris for parameterized Flask route.\n"
+#         "Try /module for module import guidance"
+#     )
+# CORS(app)
+
+# app_register_blueprint(request_api.get_blueprint())
 
 @app.route("/hello/<name>", methods=['GET'])
 def hello(name: str):
@@ -19,6 +24,30 @@ def hello(name: str):
 @app.route("/module")
 def module():
     return f"loaded from FlaskApp.package.module = {MODULE_VALUE}"
+
+SWAGGER_URL = '/swagger'  # URL for exposing Swagger UI (without trailing '/')
+API_URL = 'FlaskApp/static/swagger.json'  # Our API url (can of course be a local resource)
+
+# Call factory function to create our blueprint
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
+    API_URL,
+    config={  # Swagger UI config overrides
+        'app_name': "Test application"
+    },
+    # oauth_config={  # OAuth config. See https://github.com/swagger-api/swagger-ui#oauth2-configuration .
+    #    'clientId': "your-client-id",
+    #    'clientSecret': "your-client-secret-if-required",
+    #    'realm': "your-realms",
+    #    'appName': "your-app-name",
+    #    'scopeSeparator': " ",
+    #    'additionalQueryStringParams': {'test': "hello"}
+    # }
+)
+
+app.register_blueprint(swaggerui_blueprint)
+
+app.register_blueprint(request_api.get_blueprint())
 
 if __name__ == "__main__":
     app.run()
--- HandleApproach/__init__.py
+++ HandleApproach/__init__.py
@@ -1,7 +1,56 @@
 import azure.functions as func
 from FlaskApp import app
+from flask import Flask, request
+from flask_swagger_ui import get_swaggerui_blueprint
+
+
+
+app = Flask(__name__)
+
+@app.route('/HandleApproach')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/getString')
+def string():
+    return string
+
+
+SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
+API_URL = 'http://localhost:7071/{*route}'  # Our API url (can of course be a local resource)
+
+# Call factory function to create our blueprint
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
+    API_URL,
+    config={  # Swagger UI config overrides
+        'app_name': "Test application"
+    },
+    # oauth_config={  # OAuth config. See https://github.com/swagger-api/swagger-ui#oauth2-configuration .
+    #    'clientId': "your-client-id",
+    #    'clientSecret': "your-client-secret-if-required",
+    #    'realm': "your-realms",
+    #    'appName': "your-app-name",
+    #    'scopeSeparator': " ",
+    #    'additionalQueryStringParams': {'test': "hello"}
+    # }
+)
+
+app.register_blueprint(swaggerui_blueprint)
+
+@app.route('/form-example', methods=['GET', 'POST'])
+def form_example():
+    return '''
+              <form method="POST">
+                  <div><label>Language: <input type="text" name="language"></label></div>
+                  <div><label>Framework: <input type="text" name="framework"></label></div>
+                  <input type="submit" value="Submit">
+              </form>'''
+
 
 def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     """Each request is redirected to the WSGI handler.
     """
     return func.WsgiMiddleware(app.wsgi_app).handle(req, context)
+
+
